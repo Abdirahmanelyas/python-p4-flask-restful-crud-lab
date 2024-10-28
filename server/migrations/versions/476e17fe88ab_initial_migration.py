c5fbba9130ef1a12051d8c6ba4527b7e04176a21
Abdirahmanelyas <abdirahman.elyas@student.moringaschool.com>
2024-10-28 22:59:15 +0300

    done

--- server/migrations/versions/476e17fe88ab_initial_migration.py
+++ server/migrations/versions/476e17fe88ab_initial_migration.py
@@ -1,8 +1,8 @@
-"""create table plants
+"""Initial migration.
 
-Revision ID: 1fbfa43fdc29
-Revises: 00528bc9e4f8
-Create Date: 2022-09-26 15:43:25.150032
+Revision ID: 476e17fe88ab
+Revises: 
+Create Date: 2024-10-28 16:59:25.217950
 
 """
 from alembic import op
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '1fbfa43fdc29'
-down_revision = '00528bc9e4f8'
+revision = '476e17fe88ab'
+down_revision = None
 branch_labels = None
 depends_on = None
 
